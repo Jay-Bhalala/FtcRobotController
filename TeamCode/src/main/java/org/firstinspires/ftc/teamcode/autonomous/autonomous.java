e5fcbb9713760260d46fe49a3f19e758553ecb6b
Jeremy Chuah <JeremyChuah15@gmail.com>
2022-01-14 00:42:44 -0600

    auto code structure for test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/autonomous.java
@@ -13,7 +13,8 @@ public class autonomous extends LinearOpMode {
     Servo liftServo;
 
     @Override
-    public void runOpMode() {
+    public void runOpMode() throws InterruptedException{
+        //Map the hardware for robot
         robot = new Hardware(hardwareMap);
 
         // assigning motors
@@ -23,6 +24,100 @@ public class autonomous extends LinearOpMode {
         LB = robot.LB;
 
         //Motors for intake
+        intakeMotor = robot.intakeMotor;
+        carousel = robot.carousel;
+        liftMotor = robot.liftMotor;
+        liftServo = robot.liftServo;
+
+        waitForStart();
+
+        //Spins the carousel
+        carousel.setPower(1);
+        sleep(1000);
+
+        //backwards
+        LF.setPower(.75);
+        RF.setPower(.75);
+        LB.setPower(.75);
+        RB.setPower(.75);
+        sleep(300);
+
+        //strafe right
+        LF.setPower(-.5);
+        RF.setPower(.5);
+        LB.setPower(.5);
+        RB.setPower(-.5);
+        sleep(250);
+
+        //turn right
+        LF.setPower(.5);
+        RF.setPower(-.5);
+        LB.setPower(.5);
+        RB.setPower(-.5);
+        sleep(750);
+
+        //drive forward toward fright
+        LF.setPower(1);
+        RF.setPower(1);
+        LB.setPower(1);
+        RB.setPower(1);
+        sleep(3000);
+
+        //intake
+        intakeMotor.setPower(1);
+        sleep(1000);
+
+        //turn 180 degrees
+        LF.setPower(.5);
+        RF.setPower(-.5);
+        LB.setPower(.5);
+        RB.setPower(-.5);
+        sleep(1500);
+
+        //move forward again
+        LF.setPower(1);
+        RF.setPower(1);
+        LB.setPower(1);
+        RB.setPower(1);
+        sleep(2000);
+
+        //turn left
+        LF.setPower(-.5);
+        RF.setPower(.5);
+        LB.setPower(-.5);
+        RB.setPower(.5);
+        sleep(750);
+
+        //move forward if needed
+
+        //linear lift dump onto hub
+        liftMotor.setPower(1);
+        sleep(1000);
+        liftServo.setPosition(1);
+        sleep(1000);
+        liftMotor.setPower(-1);
+        sleep(1000);
+
+        //turn right
+        LF.setPower(.5);
+        RF.setPower(-.5);
+        LB.setPower(.5);
+        RB.setPower(-.5);
+        sleep(750);
+
+        //adjust strafe if needed
+
+        //move forward
+        LF.setPower(1);
+        RF.setPower(1);
+        LB.setPower(1);
+        RB.setPower(1);
+        sleep(1500);
+
+        telemetry.addData("path", "complete");
+        telemetry.update();
+        sleep(1000);
+
     }
 
 }
